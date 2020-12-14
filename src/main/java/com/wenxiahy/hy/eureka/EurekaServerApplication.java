c7ca41c5447729f9e733102c97349e73b1e5a329
周伟 <zhouwei@techfuser.net>
2020-12-14 10:15:50 +0800

    first commit

---
+++ src/main/java/com/wenxiahy/hy/eureka/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.wenxiahy.hy.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author zhouw
+ * @Description
+ * @Date 2020-12-13
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
